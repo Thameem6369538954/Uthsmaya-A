04c3e7db6ee4b167dfe19dfe13d6886526023874
Thameem Ansari <thameemrpp@gmail.com>
2025-04-09 10:39:38 +0530

    jias

--- src/Components/Offerbaner.jsx
+++ src/Components/Offerbaner.jsx
@@ -1,23 +1,9 @@
-import React from 'react';
-import KK from "../Images/KK.jpg";
-import Pol from "../cateimg/Pol.jpg"
+import React from 'react'
 
-const   Offerbaner = () => {
+const Offerbaner = () => {
   return (
-    <div className="w-full flex justify-center px-4">
-      <div
-        style={{ backgroundImage: `url(${Pol})` }}
-        className="min-h-[300px] sm:min-h-[350px] md:min-h-[400px] w-full max-w-7xl bg-cover bg-center rounded-lg flex items-center justify-start text-white text-center p-4"
-      >
-        {/* <div className="bg-black bg-opacity-50 rounded-lg p-6 w-full max-w-xl">
-          <h1 className="text-2xl sm:text-3xl md:text-4xl font-bold font-[Heading]">
-            Big Sale Offer!
-          </h1>
-          <p className="text-lg sm:text-xl mt-2 font-[New]">Coming Soon</p>
-        </div> */}
-      </div>
-    </div>
-  );
-};
+    <div>Offerbaner</div>
+  )
+}
 
-export default Offerbaner;
+export default Offerbaner
